57dd6a58633d12606727ab147b44ea5881e95d4e
lusopoemas <lusopoemas@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2010-01-10 12:08:42 +0000

    Subscribers - Adding to trunk

---
+++ trunk/modules/subscribers/language/arabic/blocks.php
@@ -0,0 +1,7 @@
+<?php
+define("_MB_SUBSCRIBERS_SUBSCRIBE", "ÇÔÊÑß Ýí ÇáÞÇÆãÉ ÇáÈÑíÏíÉ");
+define("_MB_SUBSCRIBERS_ENTER_COUNTRY", "ÇáÈáÏ : ");
+define("_MB_SUBSCRIBERS_ENTER_EMAIL", "ÈÑíÏß ÇáÅáßÊÑæäí");
+define("_MB_SUBSCRIBERS_ENTER_NAME", "ÇÓãß");
+define("_MB_SUBSCRIBERS_SUBMIT", "ÇÔÊÑß ÇáÂä");
+?>
---
+++ trunk/modules/subscribers/language/arabic/modinfo.php
@@ -0,0 +1,21 @@
+<?php
+define("_MI_SUBSCRIBERS_MD_NAME", "ÇáÞÇÆãÉ ÇáÈÑíÏíÉ");
+define("_MI_SUBSCRIBERS_MD_DSC", "ÈÑäÇãÌ áãÑÇÓáÉ ÇáÞÇÆãÉ ÇáÈÑíÏíÉ");
+define("_MI_SUBSCRIBERS_BLK_ADD", "ÇÔÊÑß Ýí ÇáÞÇÆãÉ ÇáÈÑíÏíÉ");
+define("_MI_SUBSCRIBERS_PAGE_INDEX", "ÇáÕÝÍÉ ÇáÑÆíÓíÉ");
+define("_MI_SUBSCRIBERS_ADMENU_USER", "ÞÇÆãÉ ÇáãÔÊÑßíä");
+define("_MI_SUBSCRIBERS_ADMENU_WAITING", "ÑÓÇÆá Ýí ÇáÅäÊÙÇÑ");
+define("_MI_SUBSCRIBERS_ADMENU_SEND", "ÅÑÓÇá ÑÓÇáÉ ÌÏíÏÉ");
+define("_MI_SUBSCRIBERS_CONF_EDITOR", "ÇáãÍÑÑ ÇáÅÝÊÑÇÖí");
+define("_MI_SUBSCRIBERS_CONF_EDITOR_DSC", "Select the editor you want to use in submit form");
+define("_MI_SUBSCRIBERS_CONF_COUNTRY", "Country");
+define("_MI_SUBSCRIBERS_CONF_COUNTRY_DSC", "Select the default country to show in subscription form");
+define("_MI_SUBSCRIBERS_CONF_FROMNAME", "From Name");
+define("_MI_SUBSCRIBERS_CONF_FROMNAME_DSC", "Enter the name that will show under 'FROM' in email header");
+define("_MI_SUBSCRIBERS_CONF_FROMEMAIL", "From Email");
+define("_MI_SUBSCRIBERS_CONF_FROMEMAIL_DSC", "Enter the email address that will show under 'FROM' in email header");
+define("_MI_SUBSCRIBERS_CONF_EMAILSPERPACK", "Emails Per Pack");
+define("_MI_SUBSCRIBERS_CONF_EMAILSPERPACK_DSC", "Number of emails to send each time");
+define("_MI_SUBSCRIBERS_CONF_TIMEBPACKS", "Time between packs");
+define("_MI_SUBSCRIBERS_CONF_TIMEBPACKS_DSC", "Interval of seconds between each pack");
+?>
