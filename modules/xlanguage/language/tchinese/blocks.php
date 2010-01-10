2f5331aa1dd600ada6f5407447d58cf2b8a84da0
lusopoemas <lusopoemas@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2010-01-10 12:13:40 +0000

    XLanguage - adding to trunk

---
+++ modules/xlanguage/language/tchinese/blocks.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: blocks.php,v 1.1.1.3 2004/11/08 03:40:00 phppp Exp $
+// Blocks
+define('_MB_XLANGUAGE_SELECT', '點擊選擇語言');
+define('_MB_XLANGUAGE_DISPLAY_METHOD', '顯示語言選項方式');
+define('_MB_XLANGUAGE_DISPLAY_FLAGLIST', '圖示列表');
+define('_MB_XLANGUAGE_DISPLAY_TEXTLIST', '文本列表');
+define('_MB_XLANGUAGE_DISPLAY_DROPDOWNLIST', '下拉菜單');
+define('_MB_XLANGUAGE_IMAGE_SEPARATOR', '圖示分隔符號');
+define('_MB_XLANGUAGE_IMAGE_PERROW', '每行圖示數');
+define('_MB_XLANGUAGE_OPTIONAL', '可選');
+?>
---
+++ modules/xlanguage/language/tchinese/modinfo.php
@@ -0,0 +1,26 @@
+<?php
+// $Id: modinfo.php,v 1.2 2004/09/27 20:37:17 phppp Exp $
+
+define("_MI_XLANGUAGE_NAME","可擴展語言管理");
+define("_MI_XLANGUAGE_DESC","可擴展的多語言管理");
+
+define("_MI_XLANGUAGE_BNAME","語言選擇");
+
+define("_MI_XLANGUAGE_ADMENU0","語種列表");
+define("_MI_XLANGUAGE_ADMENU1","添加基本語言");
+define("_MI_XLANGUAGE_ADMENU2","添加擴展語言");
+define("_MI_XLANGUAGE_ADMENU3","關於該模組");
+
+define('_MI_XLANG_AUTHOR_INFO', "Developer Information");
+define('_MI_XLANG_AUTHOR_NAME', "Developer");
+define('_MI_XLANG_AUTHOR_WEBSITE', "Developer website");
+define('_MI_XLANG_AUTHOR_EMAIL', "Developer email");
+define('_MI_XLANG_AUTHOR_CREDITS', "Credits");
+define('_MI_XLANG_MODULE_INFO', "Module Development Information");
+define('_MI_XLANG_MODULE_STATUS', "Development Status");
+define('_MI_XLANG_MODULE_DEMO', "Demo Site");
+define('_MI_XLANG_MODULE_SUPPORT', "Official support site");
+define('_MI_XLANG_RELEASE', "Release Date ");
+define('_MI_XLANG_AUTHOR_BUGFIXES', "Bug fix history");
+define('_MI_XLANG_MODULE_XOOPSVERSION', "Xoops Version");
+?>
