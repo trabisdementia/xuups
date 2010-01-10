571d2dea5bd9c282763bd7c0e9f1b91025ce80bd
lusopoemas <lusopoemas@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2010-01-10 12:03:26 +0000

    usercsv - adding to trunk

---
+++ modules/usercsv/language/english/modinfo.php
@@ -0,0 +1,5 @@
+<?php
+define("_MI_UCSV_MD_NAME", "User CSV");
+define("_MI_UCSV_MD_DSC", "This module enables you to export your user to a csv file.");
+define("_MI_UCSV_ADMIN_INDEX", "Export");
+?>
