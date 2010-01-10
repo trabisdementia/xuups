73efa916cd08b828b8e9d1b96b8e9add6ce3cd7a
lusopoemas <lusopoemas@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2010-01-10 12:03:26 +0000

    usercsv - adding to trunk

---
+++ trunk/modules/usercsv/admin/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * @copyright       The XUUPS Project http://sourceforge.net/projects/xuups/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         usercsv
+ * @since           1.0
+ * @author          trabis <lusopoemas@gmail.com>
+ * @version         $Id: index.php 0 2009-08-26 18:47:04Z trabis $
+ */
+
+include dirname(__FILE__) . '/admin_index.php';
+?>
