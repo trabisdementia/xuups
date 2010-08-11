dd0148a2f9b19d3ea3539db3a3b22f1bdbabdd4b
lusopoemas@gmail.com <lusopoemas@gmail.com@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2010-08-11 10:52:51 +0000

    mymenus - updating documentation

--- trunk/modules/mymenus/plugins/profile/language/english/profile.php
+++
@@ -1,9 +0,0 @@
-<?php
-//  Author: Trabis
-//  URL: http://www.xuups.com
-//  E-Mail: lusopoemas@gmail.com
-
-
-define('_PL_MYMENUS_PROFILE_ISFRIEND', "Is Friend");
-define('_PL_MYMENUS_PROFILE_ISNOTFRIEND', "Is Not Friend");
-
