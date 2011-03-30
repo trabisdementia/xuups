616a895f6b69d02603d08c24187a44d1884e81dc
lusopoemas@gmail.com <lusopoemas@gmail.com@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2011-03-30 18:42:40 +0000

    Subscribers - Fixing captcha bug on block, added missing block defines, updating version

--- trunk/modules/subscribers/blocks/subscribers_add.php
+++ trunk/modules/subscribers/blocks/subscribers_add.php
@@ -6,6 +6,7 @@ if (!defined("XOOPS_ROOT_PATH")) {
 
 function subscribers_add_show($options)
 {
+    global $xoopsUser;
     include_once XOOPS_ROOT_PATH . '/modules/subscribers/include/functions.php';
     subscribers_sendEmails();
 
@@ -16,7 +17,11 @@ function subscribers_add_show($options)
     $block['selected'] = $config['country'];
     array_shift($block['countries']);
     $sub_captcha = $config['captcha'];
-	if($xoopsUser) {$block['captcha'] = ($sub_captcha==2)?0:1;}else {$block['captcha'] = ($sub_captcha==3)?0:1;}
+    if (is_object($xoopsUser)) {
+        $block['captcha'] = $sub_captcha == 2 ? 0 : 1;
+    } else {
+        $block['captcha'] = $sub_captcha == 3 ? 0 : 1;
+    }
     return $block;
 }
 ?>
--- trunk/modules/subscribers/language/english/blocks.php
+++ trunk/modules/subscribers/language/english/blocks.php
@@ -3,5 +3,6 @@ define("_MB_SUBSCRIBERS_SUBSCRIBE", "Subscribe to our mailing list");
 define("_MB_SUBSCRIBERS_ENTER_COUNTRY", "Country : ");
 define("_MB_SUBSCRIBERS_ENTER_EMAIL", "Email : ");
 define("_MB_SUBSCRIBERS_ENTER_NAME", "Name : ");
+define("_MB_SUBSCRIBERS_ENTER_SECURITY", "AntiSpam : ");
 define("_MB_SUBSCRIBERS_SUBMIT", "Subscribe now !!!");
 ?>
