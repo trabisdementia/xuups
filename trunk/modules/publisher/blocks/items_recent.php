7a7c64e3228961c8a098d5c1e0ad45983d9fdca5
lusopoemas@gmail.com <lusopoemas@gmail.com@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2011-04-02 14:13:11 +0000

    publisher - updating block items spot

--- trunk/modules/publisher/blocks/items_recent.php
+++ trunk/modules/publisher/blocks/items_recent.php
@@ -90,7 +90,7 @@ function publisher_items_recent_edit($options)
 
     $form = new PublisherBlockForm();
 
-    $catEle = new XoopsFormLabel(_MB_PUBLISHER_SELECTCAT, publisher_createCategorySelect($options[0]), 'options[0]');
+    $catEle = new XoopsFormLabel(_MB_PUBLISHER_SELECTCAT, publisher_createCategorySelect($options[0], 0, true, 'options[0]'));
     $orderEle = new XoopsFormSelect(_MB_PUBLISHER_ORDER, 'options[1]', $options[1]);
     $orderEle->addOptionArray(array(
         'datesub' => _MB_PUBLISHER_DATE,
