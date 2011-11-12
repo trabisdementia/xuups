8f44e6b7df686a6336186e5f0a68fa39429d1ae0
lusopoemas@gmail.com <lusopoemas@gmail.com@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2011-11-12 16:06:41 +0000

    removing old files

--- branches/2.6/newbb/class/xoopsformloader.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: xoopsformloader.php 2169 2008-09-23 13:37:10Z phppp $
-if (!defined('XOOPS_ROOT_PATH')) {
-    exit();
-}
-
-include_once XOOPS_ROOT_PATH."/class/xoopsformloader.php";
-?>
