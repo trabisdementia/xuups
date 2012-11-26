28806d94c459bc6c0620319ab98234f1f45cecd2
lusopoemas@gmail.com <lusopoemas@gmail.com@042f83fe-514c-0410-bc4e-0bfa52e0c48f>
2012-11-26 23:16:18 +0000

    Publisher - Removing registry class

--- trunk/modules/publisher/include/common.php
+++ trunk/modules/publisher/include/common.php
@@ -8,7 +8,6 @@
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 /**
  *  Publisher class
  *
@@ -20,27 +19,23 @@
  * @author          trabis <lusopoemas@gmail.com>
  * @version         $Id$
  */
-
 defined("XOOPS_ROOT_PATH") or die("XOOPS root path not defined");
-/** @define "PUBLISHER_DIRNAME" "publisher" */
+
 define("PUBLISHER_DIRNAME", basename(dirname(dirname(__FILE__))));
 define("PUBLISHER_URL", XOOPS_URL . '/modules/' . PUBLISHER_DIRNAME);
 define("PUBLISHER_IMAGES_URL", PUBLISHER_URL . '/images');
 define("PUBLISHER_ADMIN_URL", PUBLISHER_URL . '/admin');
 define("PUBLISHER_UPLOADS_URL", XOOPS_URL . '/uploads/' . PUBLISHER_DIRNAME);
-
 define("PUBLISHER_ROOT_PATH", XOOPS_ROOT_PATH . '/modules/' . PUBLISHER_DIRNAME);
 define("PUBLISHER_UPLOADS_PATH", XOOPS_ROOT_PATH . '/uploads/' . PUBLISHER_DIRNAME);
 
 xoops_loadLanguage('common', PUBLISHER_DIRNAME);
 
-/** @define "PUBLISHER_DIRNAME" "publisher" */
 include_once PUBLISHER_ROOT_PATH . '/include/functions.php';
 include_once PUBLISHER_ROOT_PATH . '/include/constants.php';
 include_once PUBLISHER_ROOT_PATH . '/include/seo_functions.php';
 include_once PUBLISHER_ROOT_PATH . '/class/metagen.php';
 include_once PUBLISHER_ROOT_PATH . '/class/session.php';
-include_once PUBLISHER_ROOT_PATH . '/class/registry.php';
 include_once PUBLISHER_ROOT_PATH . '/class/publisher.php';
 include_once PUBLISHER_ROOT_PATH . '/class/request.php';
 
@@ -52,11 +47,8 @@ global $publisher_isAdmin;
 
 // Load only if module is installed
 if (is_object($publisher->getModule())) {
-
     // Find if the user is admin of the module
     $publisher_isAdmin = publisher_userIsAdmin();
-
     // get current page
     $publisher_current_page = publisher_getCurrentPage();
 }
-?>
